af5c5bb257e2ec73eb47ebc5d52208d0b44a4579
asraful808088 <asraful808088@gmail.com>
2024-10-19 09:48:40 -0700

    commit init

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const {listClickByClass}  = require('./utility/listClick/listClickByClass')
 const fs = require("fs");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto("https://web.telegram.org/a/", { waitUntil: "networkidle2" });
-  await loadLocalStorageData(page)
-  await page.reload({ waitUntil: "networkidle2" });
-  await wait(10000)
-  await page.goto("https://web.telegram.org/k/#@wsotp_bot", { waitUntil: "networkidle2" });
+  
 
   
 
