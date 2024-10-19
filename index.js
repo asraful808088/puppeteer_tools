d64f6919e405105b7a8eedbc529d2d5f513c3667
asraful808088 <asraful808088@gmail.com>
2024-10-19 09:54:12 -0700

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const puppeteer = require("puppeteer");
-const { loadLocalStorageData } = require("./cookie/load_cookie");
-// const { save_cookie } = require("./cookie/save_cookie");
 const { setTimeout: wait } = require("timers/promises");
 const {listClickByClass}  = require('./utility/listClick/listClickByClass')
 const fs = require("fs");
